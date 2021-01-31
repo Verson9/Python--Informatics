3700fa8ceaf0481484e705ff9220547c96db4fdb
Krystianwab <78229894+Krystianwab@users.noreply.github.com>
2021-01-31 09:39:56 +0100

    Update fotoradar.py

--- year_1/chyla_cw/wadas/fotoradar.py
+++ year_1/chyla_cw/wadas/fotoradar.py
@@ -1,13 +1,65 @@
-nr = input('Podaj numer rejestracyny pojazdu')
-tp = input('Podaj typ pojazdu')
-po = input('Podaj przejechaną odległość w metrach')
-fp = input('Podaj godzinę przejazdu przez pierwszy fotoradar')
-fk = input('Podaj godzinę przejazdu przez drugi fotoradar')
+nr, tp, po, fp, fk=input("" ).split(" ")
 fph = fp[0]+fp[1]
 fpm = fp[3]+fp[4]
 fkh = fk[0]+fk[1]
 fkm = fk[3]+fk[4]
-if tp == "C" or tp == "S" and int(fph)<24 and int(fpm)<60 and int(fkh)<24 and int(fkm)<60:
+
+
+litery = {
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "R",
+    "S",
+    "T",
+    "U",
+    "W",
+    "Y",
+    "Z",
+    "X",
+    "Q",
+    "V"
+}
+liczby = {
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "0"
+}
+liczbya = {
+    "0",
+    "1",
+    "2"
+}
+liczbyb = {
+    "0",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5"
+}
+
+if fp[0] in list(liczbya) and fp[1] in list(liczby) and fp[2] == ":" and fp[3] in list(liczbyb) and fp[4] in list(liczby) and fk[0] in list(liczbya) and fk[1] in list(liczby) and fk[2] == ":" and fk[3] in list(liczbyb) and fk[4] in list(liczby) and int(fph)<24 and int(fpm)<60 and int(fkh)<24 and int(fkm)<60 and nr[0] in list(litery) and nr[1] in list(litery) and nr[2] in list(liczby) and nr[3] in list(liczby) and nr[4] in list(liczby) and nr[5] in list(liczby) and tp == "S" or tp == "C":
     s=int(po)
     hfp = int(fph)
     mfp = int(fpm)
@@ -24,17 +76,17 @@ if tp == "C" or tp == "S" and int(fph)<24 and int(fpm)<60 and int(fkh)<24 and in
     godziny_fk = minuty_fk / 60
     v=s/1000/(godziny_fk-godziny_fp)
 
-    if round(v, 2)>140 and tp=="S":
+
+    if round(v, 2)>80 and tp == "C":
         mandat="M"
-    else:
-        mandat="."
-    if round(v, 2)>80 and tp=="C":
+    elif round(v, 2)>140 and tp == "S":
         mandat="M"
     else:
         mandat="."
 
-
-    print(nr," ", mandat," ", round(v, 2),"km/h")
+    print(nr," ", mandat," ", round(v, 2))
 else:
     print("BLAD")
+
+
 
